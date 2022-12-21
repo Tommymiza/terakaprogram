115a48ed7f907ab5875b74c9d13c71df6ed93de8
Tommymiza <tommymiza20@gmail.com>
2022-12-21 15:10:01 +0300

    Footer design

--- src/components/outils/Footer.jsx
+++ src/components/outils/Footer.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import "../../styles/footer.scss";
-import { FacebookRounded, LinkedIn } from "@mui/icons-material";
+import { FacebookRounded, LinkedIn, WhatsApp } from "@mui/icons-material";
 
-const Footer = ({t}) => {
+const Footer = ({ t }) => {
   return (
     <div id="footer">
       <div>
@@ -13,18 +13,27 @@ const Footer = ({t}) => {
         />
         <div>
           <h3>{t("navbar.7")}</h3>
-          <h5>TERAKA MADAGASCAR</h5>
+          <h5>iTERAKA Madagascar</h5>
           <p>212 route Circulaire Ambanidia</p>
           <p>Antananarivo 101</p>
           <p>MADAGASCAR</p>
           <p>+261 (0) 32 15 951 92</p>
           <p>+261 (0) 34 88 14 236</p>
-          <a href="mailto:info@TERAKA.org">info@TERAKA.org</a>
-          <h5>iTERAKA France</h5>
-          <p>21 rue du Col de Dyane</p>
-          <p>78180 Montigny le Bretonneux</p>
-          <p>FRANCE</p>
-          <p>+ 33(0) 6 44 95 15 54</p>
+          <h5>iTERAKA Europe</h5>
+          <div>
+            <div>
+              <p>21 rue du Col de Dyane</p>
+              <p>78180 Paris sud</p>
+              <p>FRANCE</p>
+              <p>+ 33 (0) 6 44 95 15 54</p>
+            </div>
+            <div>
+              <p>31 rue du chateau d'eau</p>
+              <p>1180 Bruxelles</p>
+              <p>BELGIQUE</p>
+              <p>+32 (0) 493 96 45 03</p>
+            </div>
+          </div>
         </div>
         <div>
           <h3>{t("footer.0")}</h3>
@@ -42,6 +51,19 @@ const Footer = ({t}) => {
             }}
           />
           <LinkedIn
+            sx={{
+              marginTop: 2,
+              marginRight: 2,
+              fontSize: "50px",
+              transition: "0.2s",
+              cursor: "pointer",
+              "&:hover": {
+                transform: "scale(1.1)",
+                color: "white",
+              },
+            }}
+          />
+          <WhatsApp
             sx={{
               marginTop: 2,
               fontSize: "50px",
